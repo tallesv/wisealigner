c8866ac98fb0172197c8428c27ef4993ce2f4146
talles <talles17.a@hotmail.com>
2022-01-30 02:09:56 -0300

    first commit

---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export default extendTheme({
+  colors: {},
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.300',
+      },
+    },
+  },
+});
