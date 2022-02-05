0aaa210dd5033a7992ca6de221ea649a4458703c
talles <talles17.a@hotmail.com>
2022-02-05 13:02:40 -0300

    fix: change login page layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,66 @@
-import { Flex, Center, VStack, Text, Button } from '@chakra-ui/react';
-import Image from 'next/image';
+import {
+  Box,
+  Flex,
+  VStack,
+  Text,
+  Input as ChakraInput,
+} from '@chakra-ui/react';
+import Link from 'next/link';
 import { Input } from '../components/Form/input';
+import { Button } from '../components/Button';
 
 export default function Login() {
   return (
-    <Center h="100vh">
-      <Flex w={700} p={[2, 10]} mb={[20, 40]} direction="column">
-        <Image src="/vercel.svg" width={100} height={50} />
-        <VStack spacing={7} px={[7, 20]} align="stretch" mt={10}>
-          <Text fontSize={24} fontWeight={900}>
-            Entrar
+    <Box>
+      <VStack pt={[7, 14]} mx="auto" maxW={1000} spacing={10}>
+        <Box h={6} w={[358, 540]} pl={[4, 8]} align="left">
+          <Text fontSize={[24]} fontWeight={800}>
+            Untitleproject
           </Text>
-          <Input name="email" type="email" placeholder="Email" />
-          <Input name="password" type="password" placeholder="Senha" />
-          <Button
-            size="lg"
-            fontSize="lg"
-            letterSpacing={1.2}
-            colorScheme="blue"
-          >
-            Entrar
-          </Button>
+        </Box>
+
+        <Box bgColor="white" w={[358, 540]} h={[436]} borderRadius={5}>
+          <Flex px={[1, 2]} py={[2, 3]} direction="column">
+            <VStack spacing={7} px={[7, 16]} align="stretch" mt={10}>
+              <Text fontSize={24} fontWeight={800}>
+                Entrar
+              </Text>
+              <Input name="email" type="email" label="Email" />
+              <Box>
+                <Flex justifyContent="space-between">
+                  <Text fontWeight={700}>Senha</Text>
+                  <Text
+                    fontWeight={700}
+                    as="a"
+                    color="purple.550"
+                    _hover={{
+                      color: 'blue.450',
+                    }}
+                  >
+                    <Link href="/sign-up">Esqueceu a senha?</Link>
+                  </Text>
+                </Flex>
+                <Input name="password" type="password" mt={2} />
+              </Box>
+              <Button>Entrar</Button>
+            </VStack>
+          </Flex>
+        </Box>
+
+        <Flex w={[358, 540]} pl={[2, 4]} fontSize={14} align="left">
           <Text>Não possui conta?</Text>
-        </VStack>
-      </Flex>
-    </Center>
+          <Text
+            as="a"
+            ml={2}
+            color="purple.550"
+            _hover={{
+              color: 'blue.450',
+            }}
+          >
+            <Link href="/sign-up">Criar conta</Link>
+          </Text>
+        </Flex>
+      </VStack>
+    </Box>
   );
 }
