9c67d1562ee3da9cccdbca15956866c7b2eaca88
talles <talles17.a@hotmail.com>
2022-02-20 19:39:50 -0300

    feat: add chakra-ui steps to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { StepsStyleConfig as Steps } from 'chakra-ui-steps';
 import { useRouter } from 'next/router';
 import theme from '../../styles/theme';
 import { Layout } from '../components/Layout';
@@ -9,8 +10,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
   const pagesWithoutLayout = ['/login', '/sign-up'];
 
+  const themeExtented = extendTheme({
+    ...theme,
+    components: {
+      Steps,
+    },
+  });
+
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={themeExtented}>
       {pagesWithoutLayout.includes(pathname) ? (
         <Component {...pageProps} />
       ) : (
