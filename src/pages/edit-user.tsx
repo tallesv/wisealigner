dc3abcdc11e0c55f77fd9a05c32bb349a0c7473e
talles <talles17.a@hotmail.com>
2022-02-20 19:38:19 -0300

    feat: add remove avatar button

--- src/pages/edit-user.tsx
+++ src/pages/edit-user.tsx
@@ -10,7 +10,12 @@ import {
   Select,
   FormControl,
   FormLabel,
+  AvatarBadge,
+  Text,
+  IconButton,
+  Tooltip,
 } from '@chakra-ui/react';
+import { FiXCircle } from 'react-icons/fi';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -98,216 +103,220 @@ function EditUser() {
   }
 
   return (
-    <Box bgColor="white" w="100%" borderRadius={8}>
-      <Box
-        maxWidth={1000}
-        mx="auto"
-        p={[6, 8]}
-        as="form"
-        onSubmit={handleSubmit(handleEditUser)}
-      >
-        <Flex justifyContent="space-between">
-          <Heading size="lg">Editar dados</Heading>
+    <Box
+      maxWidth={1000}
+      mx="auto"
+      p={[6, 8]}
+      as="form"
+      onSubmit={handleSubmit(handleEditUser)}
+    >
+      <Flex justifyContent="space-between">
+        <Heading size="lg">Editar dados</Heading>
 
-          <Button
-            bgColor="white"
-            _hover={{ bgColor: 'white' }}
-            color="blue.450"
-            variant="outline"
-            size="md"
+        <Button
+          bgColor="white"
+          _hover={{ bgColor: 'white' }}
+          color="blue.450"
+          variant="outline"
+          size="md"
+        >
+          Alterar senha
+        </Button>
+      </Flex>
+
+      <Divider my="6" borderColor="gray.800" />
+
+      <Flex direction="column">
+        <VStack spacing={[8]} px={10}>
+          <Stack
+            w="100%"
+            direction={['column', 'column', 'row']}
+            justifyContent="space-between"
           >
-            Alterar senha
-          </Button>
-        </Flex>
+            <Heading mb={5} as="h3" size="md">
+              Foto de perfil
+            </Heading>
+            <Flex align="center" justify="space-evenly">
+              <Avatar
+                size={isWideVersion ? 'xl' : 'lg'}
+                name="Talles"
+                src="https://github.com/tallesv.png"
+              >
+                <AvatarBadge boxSize={10} bg="red.500">
+                  <Tooltip label="Remover avatar" aria-label="Remove avatar">
+                    <IconButton
+                      colorScheme="red.500"
+                      aria-label="Remove avatar"
+                      icon={<FiXCircle />}
+                    />
+                  </Tooltip>
+                </AvatarBadge>
+              </Avatar>
 
-        <Divider my="6" borderColor="gray.800" />
+              <Button
+                bgColor="white"
+                _hover={{ bgColor: 'white' }}
+                color="blue.450"
+                variant="outline"
+                size={isWideVersion ? 'md' : 'sm'}
+                ml={5}
+              >
+                Alterar avatar
+              </Button>
+            </Flex>
+          </Stack>
 
-        <Flex direction="column">
-          <VStack spacing={[8]} px={10}>
-            <Stack
-              w="100%"
-              direction={['column', 'column', 'row']}
-              justifyContent="space-between"
-            >
-              <Heading mb={5} as="h3" size="md">
-                Foto de perfil
-              </Heading>
-              <Flex align="center" justify="space-evenly">
-                <Avatar
-                  size={isWideVersion ? 'xl' : 'lg'}
-                  name="Talles"
-                  src="https://github.com/tallesv.png"
-                />
+          <Divider borderColor="gray.300" />
 
-                <Button
-                  bgColor="white"
-                  _hover={{ bgColor: 'white' }}
-                  color="blue.450"
-                  variant="outline"
-                  size={isWideVersion ? 'md' : 'sm'}
-                  ml={5}
-                >
-                  Alterar avatar
-                </Button>
-              </Flex>
-            </Stack>
-
-            <Divider borderColor="gray.300" />
-
-            <Stack
-              w="100%"
-              direction={['column', 'column', 'row']}
-              justifyContent="space-between"
-            >
-              <Heading mb={5} as="h3" size="md">
-                Dados pessoais
-              </Heading>
-              <VStack maxW={400} w="100%" spacing={3}>
-                <Input
-                  label="Nome"
-                  size={inputSize}
-                  error={errors.name}
-                  {...register('name')}
-                />
-                <Input
-                  label="Sobrenome"
-                  size={inputSize}
-                  error={errors.last_name}
-                  {...register('last_name')}
-                />
-                <Input
-                  label="Email"
-                  type="email"
-                  size={inputSize}
-                  error={errors.email}
-                  {...register('email')}
-                />
-                <Input
-                  label="CPF"
-                  size={inputSize}
-                  error={errors.cpf}
-                  {...register('cpf')}
-                />
-                <Input
-                  label="Fone Fixo"
-                  size={inputSize}
-                  error={errors.fixed_phone}
-                  {...register('fixed_phone')}
-                />
-                <Input
-                  label="Celular"
-                  size={inputSize}
-                  error={errors.phone}
-                  {...register('phone')}
-                />
-              </VStack>
-            </Stack>
-
-            <Divider borderColor="gray.300" />
-
-            <Stack
-              w="100%"
-              direction={['column', 'column', 'row']}
-              justifyContent="space-between"
-            >
-              <Heading mb={5} as="h3" size="md">
-                Dados da clínica
-              </Heading>
-              <VStack maxW={400} w="100%" spacing={3}>
-                <Input
-                  label="Clínica"
-                  size={inputSize}
-                  {...register('clinic')}
-                />
-                <Input label="CRO" size={inputSize} {...register('cro')} />
-                <FormControl>
-                  <FormLabel fontWeight={700}>Estado do CRO</FormLabel>
-                  <Select size={inputSize} {...register('cro_state')}>
-                    <option key="--" value={undefined}>
-                      {' '}
-                    </option>
-                    {states.map(state => (
-                      <option key={state} value={state}>
-                        {state}
-                      </option>
-                    ))}
-                  </Select>
-                </FormControl>
-              </VStack>
-            </Stack>
-
-            <Divider borderColor="gray.300" />
-
-            <Stack
-              w="100%"
-              direction={['column', 'column', 'row']}
-              justifyContent="space-between"
-            >
-              <Heading mb={5} as="h3" size="md">
-                Endereço
-              </Heading>
-              <VStack maxW={400} w="100%" spacing={3}>
-                <Input
-                  label="CEP"
-                  size={inputSize}
-                  type="number"
-                  {...register('cep')}
-                  onChange={e => handleFillAddress(e.target.value)}
-                />
-                <FormControl>
-                  <FormLabel fontWeight={700}>Estado</FormLabel>
-                  <Select size={inputSize} {...register('state')}>
-                    <option key="--" value={undefined}>
-                      {' '}
-                    </option>
-                    {states.map(state => (
-                      <option key={state} value={state}>
-                        {state}
-                      </option>
-                    ))}
-                  </Select>
-                </FormControl>
-                <Input label="Cidade" size={inputSize} {...register('city')} />
-                <Input
-                  label="Bairro"
-                  size={inputSize}
-                  {...register('neighborhood')}
-                />
-                <Input
-                  label="Endereço"
-                  size={inputSize}
-                  {...register('street')}
-                />
-                <Input
-                  label="Complemento"
-                  size={inputSize}
-                  {...register('complement')}
-                />
-                <Input
-                  label="Número"
-                  size={inputSize}
-                  {...register('home_number')}
-                />
-              </VStack>
-            </Stack>
-
-            <Divider borderColor="gray.300" />
-          </VStack>
-        </Flex>
-
-        <Flex justify="flex-end" mt={6}>
-          <Button
-            bgColor="white"
-            _hover={{ bgColor: 'white' }}
-            color="blue.450"
-            variant="outline"
-            mr={5}
+          <Stack
+            w="100%"
+            direction={['column', 'column', 'row']}
+            justifyContent="space-between"
           >
-            Cancelar
-          </Button>
-          <Button type="submit">Salvar</Button>
-        </Flex>
-      </Box>
+            <Heading mb={5} as="h3" size="md">
+              Dados pessoais
+            </Heading>
+            <VStack maxW={400} w="100%" spacing={3}>
+              <Input
+                label="Nome"
+                size={inputSize}
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                label="Sobrenome"
+                size={inputSize}
+                error={errors.last_name}
+                {...register('last_name')}
+              />
+              <Input
+                label="Email"
+                type="email"
+                size={inputSize}
+                error={errors.email}
+                {...register('email')}
+              />
+              <Input
+                label="CPF"
+                size={inputSize}
+                error={errors.cpf}
+                {...register('cpf')}
+              />
+              <Input
+                label="Fone Fixo"
+                size={inputSize}
+                error={errors.fixed_phone}
+                {...register('fixed_phone')}
+              />
+              <Input
+                label="Celular"
+                size={inputSize}
+                error={errors.phone}
+                {...register('phone')}
+              />
+            </VStack>
+          </Stack>
+
+          <Divider borderColor="gray.300" />
+
+          <Stack
+            w="100%"
+            direction={['column', 'column', 'row']}
+            justifyContent="space-between"
+          >
+            <Heading mb={5} as="h3" size="md">
+              Dados da clínica
+            </Heading>
+            <VStack maxW={400} w="100%" spacing={3}>
+              <Input label="Clínica" size={inputSize} {...register('clinic')} />
+              <Input label="CRO" size={inputSize} {...register('cro')} />
+              <FormControl>
+                <FormLabel fontWeight={700}>Estado do CRO</FormLabel>
+                <Select size={inputSize} {...register('cro_state')}>
+                  <option key="--" value={undefined}>
+                    {' '}
+                  </option>
+                  {states.map(state => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+            </VStack>
+          </Stack>
+
+          <Divider borderColor="gray.300" />
+
+          <Stack
+            w="100%"
+            direction={['column', 'column', 'row']}
+            justifyContent="space-between"
+          >
+            <Heading mb={5} as="h3" size="md">
+              Endereço
+            </Heading>
+            <VStack maxW={400} w="100%" spacing={3}>
+              <Input
+                label="CEP"
+                size={inputSize}
+                type="number"
+                {...register('cep')}
+                onChange={e => handleFillAddress(e.target.value)}
+              />
+              <FormControl>
+                <FormLabel fontWeight={700}>Estado</FormLabel>
+                <Select size={inputSize} {...register('state')}>
+                  <option key="--" value={undefined}>
+                    {' '}
+                  </option>
+                  {states.map(state => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <Input label="Cidade" size={inputSize} {...register('city')} />
+              <Input
+                label="Bairro"
+                size={inputSize}
+                {...register('neighborhood')}
+              />
+              <Input
+                label="Endereço"
+                size={inputSize}
+                {...register('street')}
+              />
+              <Input
+                label="Complemento"
+                size={inputSize}
+                {...register('complement')}
+              />
+              <Input
+                label="Número"
+                size={inputSize}
+                {...register('home_number')}
+              />
+            </VStack>
+          </Stack>
+
+          <Divider borderColor="gray.300" />
+        </VStack>
+      </Flex>
+
+      <Flex justify="flex-end" mt={6}>
+        <Button
+          bgColor="white"
+          _hover={{ bgColor: 'white' }}
+          color="blue.450"
+          variant="outline"
+          mr={5}
+        >
+          Cancelar
+        </Button>
+        <Button type="submit">Salvar</Button>
+      </Flex>
     </Box>
   );
 }
