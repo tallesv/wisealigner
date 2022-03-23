20356bbc8674fd2aefde1e6214ea3cc5e5545f4c
talles <talles17.a@hotmail.com>
2022-03-23 01:27:15 -0300

    feat: add pagination

---
+++ src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@chakra-ui/react';
+
+interface PaginationItemProps {
+  number: number;
+  isCurrent?: boolean;
+  onPageChange: (page: number) => void;
+}
+
+export function PaginationItem({
+  isCurrent = false,
+  number,
+  onPageChange,
+}: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="purple"
+        disabled
+        _disabled={{
+          bg: 'purple.550',
+        }}
+      >
+        {number}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      size="sm"
+      fontSize="xs"
+      width="4"
+      bg="gray.700"
+      _hover={{
+        bg: 'gray.500',
+      }}
+      onClick={() => onPageChange(number)}
+    >
+      {number}
+    </Button>
+  );
+}
