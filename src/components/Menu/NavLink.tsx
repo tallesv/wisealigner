80c42ec91757f906dcb17d1417339b36d0881580
talles <talles17.a@hotmail.com>
2022-03-29 20:52:44 -0300

    fix: menu selection

--- src/components/Menu/NavLink.tsx
+++ src/components/Menu/NavLink.tsx
@@ -15,7 +15,7 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <ActiveLink href={href} passHref>
+    <ActiveLink shouldMatchExactHref href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
