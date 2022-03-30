cd85b0102241e95aaaf68231eb80858c3021c4a6
talles <talles17.a@hotmail.com>
2022-03-29 21:59:42 -0300

    feat: cases table

---
+++ src/pages/cases-table.tsx
@@ -0,0 +1,106 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Avatar,
+  Text,
+  Tooltip,
+  IconButton,
+  Spinner,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import { RiDeleteBin2Line, RiEditLine } from 'react-icons/ri';
+import api from '../client/api';
+import { Pagination } from '../components/Pagination';
+
+function CaseTable() {
+  const [page, setPage] = useState(1);
+  const [isLoadindCase, setIsLoadingCases] = useState(false);
+  const [cases, setCases] = useState<NewCaseType[]>([]);
+
+  const { push } = useRouter();
+
+  useEffect(() => {
+    async function loadCases() {
+      setIsLoadingCases(true);
+      const response = await api.get('/requests');
+      setCases(response.data);
+      setIsLoadingCases(false);
+    }
+
+    loadCases();
+  }, []);
+
+  return (
+    <Box p={[6, 8]}>
+      <Flex mx="auto">
+        <Heading size="lg">
+          Casos
+          {isLoadindCase && <Spinner ml={5} />}
+        </Heading>
+      </Flex>
+      <Table variant="simple" mt={5}>
+        <Thead>
+          <Tr bgColor="gray.200">
+            <Th>Nome</Th>
+            <Th>Email</Th>
+            <Th>Data</Th>
+            <Th isNumeric>Opções</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {cases.slice((page - 1) * 10, page * 10).map(caseItem => (
+            <Tr key={caseItem.id}>
+              <Td>
+                <Flex align="center">
+                  <Avatar
+                    size="md"
+                    name="teste"
+                    src={caseItem.dados_do_paciente.avatar}
+                  />
+                  <Text ml={3}>{caseItem.dados_do_paciente.nome_completo}</Text>
+                </Flex>
+              </Td>
+              <Td>{caseItem.dados_do_paciente.genero}</Td>
+              <Td>{caseItem.dados_do_paciente.data_de_nascimento}</Td>
+              <Td isNumeric>
+                <Tooltip label="Deletar caso" aria-label="delete case">
+                  <IconButton
+                    aria-label="Delte case"
+                    bgColor="white"
+                    icon={<RiDeleteBin2Line />}
+                  />
+                </Tooltip>
+                <Tooltip label="Editar caso" aria-label="edit case">
+                  <IconButton
+                    aria-label="Edit case"
+                    bgColor="white"
+                    icon={<RiEditLine />}
+                    ml={3}
+                    onClick={() => push(`/case/${caseItem.id}`)}
+                  />
+                </Tooltip>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+
+      <Pagination
+        totalCountOfRegisters={cases.length}
+        currentPage={page}
+        onPageChange={setPage}
+      />
+    </Box>
+  );
+}
+
+export default CaseTable;
