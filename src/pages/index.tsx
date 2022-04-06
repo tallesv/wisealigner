3d9b255b1caf63eae0d03024627c967c9d5674f2
talles <talles17.a@hotmail.com>
2022-04-06 00:05:58 -0300

    fix: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,31 @@
+import { Box, Flex, Heading, Image, Divider } from '@chakra-ui/react';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box p={[6, 8]}>
+      <Flex mx="auto">
+        <Heading size="lg">Seja bem vindo!</Heading>
+      </Flex>
+
+      <Divider my="6" />
+
+      <Box mt={10}>
+        <a
+          href="https://www.youtube.com/watch?v=q6j8WWWgNMQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image mx="auto" src="/images/aviso-viewer.jpg" />
+        </a>
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
 
 export const getServerSideProps = withSSRAuth(async () => {
-  return {
-    redirect: {
-      destination: '/cases-table',
-      permanent: false,
-    },
-  };
-
   return {
     props: {},
   };
